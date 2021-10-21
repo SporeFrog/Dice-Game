5eed51fe713f5215f2cdfe205793a5d3dc75650e
Aaron <anaugustchill@gmail.com>
2021-10-21 19:57:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+
+function diceRoll1(){
+  var result1 = (Math.floor(Math.random()*6) + 1);
+}
+
+function diceRoll2(){
+  var result2 = (Math.floor(Math.random()*6) + 1);
+}
+
+function diceGame(){
+  var result1 = (Math.floor(Math.random()*6) + 1);
+  var result2 = (Math.floor(Math.random()*6) + 1);
+
+  if(result1 === 1){
+
+  }else if(result1 === 2){
+    
+    
+  }else if(result1 === 3){
+    
+  }else if(result1 === 4){
+    
+  }else if(result1 === 5){
+    
+  }else if(result1 === 6){
+    
+  }
+
+  // if(result2 === 1){
+
+  // }else if(result2 === 2){
+    
+  // }else if(result2 === 3){
+
+  // }else if(result2 === 4){
+
+  // }else if(result2 === 5){
+
+  // }else if(result2 === 6){
+    
+  // }
+
+}
