5afc916be370451cfa3202b1432c94fcf39ce2cb
Aaron <anaugustchill@gmail.com>
2021-10-21 20:14:39 -0400

    commit

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
 
-function diceRoll1(){
-  var result1 = (Math.floor(Math.random()*6) + 1);
-}
-
-function diceRoll2(){
-  var result2 = (Math.floor(Math.random()*6) + 1);
-}
-
 function diceGame(){
   var result1 = (Math.floor(Math.random()*6) + 1);
   var result2 = (Math.floor(Math.random()*6) + 1);
 
   if(result1 === 1){
-
+    document.querySelector(".dice1").innerHTML = "<i class='fas fa-dice-one'></i>";
   }else if(result1 === 2){
-    
-    
+    document.querySelector(".dice1").innerHTML = "<i class='fas fa-dice-two'></i>";
   }else if(result1 === 3){
-    
+    document.querySelector(".dice1").innerHTML = "<i class='fas fa-dice-three'></i>";
   }else if(result1 === 4){
-    
+    document.querySelector(".dice1").innerHTML = "<i class='fas fa-dice-four'></i>";
   }else if(result1 === 5){
-    
+    document.querySelector(".dice1").innerHTML = "<i class='fas fa-dice-five'></i>";
   }else if(result1 === 6){
-    
+    document.querySelector(".dice1").innerHTML = "<i class='fas fa-dice-six'></i>";
   }
 
-  // if(result2 === 1){
+  if(result2 === 1){
+    document.querySelector(".dice2").innerHTML = "<i class='fas fa-dice-one'></i>";
+  }else if(result2 === 2){
+    document.querySelector(".dice2").innerHTML = "<i class='fas fa-dice-two'></i>";
+  }else if(result2 === 3){
+    document.querySelector(".dice2").innerHTML = "<i class='fas fa-dice-three'></i>";
+  }else if(result2 === 4){
+    document.querySelector(".dice2").innerHTML = "<i class='fas fa-dice-four'></i>";
+  }else if(result2 === 5){
+    document.querySelector(".dice2").innerHTML = "<i class='fas fa-dice-five'></i>";
+  }else if(result2 === 6){
+    document.querySelector(".dice2").innerHTML = "<i class='fas fa-dice-six'></i>";
+  }
 
-  // }else if(result2 === 2){
-    
-  // }else if(result2 === 3){
+}
 
-  // }else if(result2 === 4){
-
-  // }else if(result2 === 5){
-
-  // }else if(result2 === 6){
-    
-  // }
-
+function test(){
+  document.querySelector(".dice1").innerHTML = "<i class='fas fa-dice-three'></i>";
 }
